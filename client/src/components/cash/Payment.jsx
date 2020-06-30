24402621ad1db83bc0e77a1478afce745cbe7a79
ruly yakovson <iyakovso@g.jct.ac.il>
2020-06-30 21:48:09 +0300

    fix 401 issue

--- client/src/components/cash/Payment.jsx
+++ client/src/components/cash/Payment.jsx
@@ -1,15 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
 import PaymentIcon from '@material-ui/icons/Payment';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import {isEmpty} from "lodash";
-import {connect} from "react-redux";
-import {createAccount} from "../../actions/loginActions";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -26,85 +21,76 @@ const useStyles = makeStyles((theme) => ({
     },
     form: {
         width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(3),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
+        marginTop: theme.spacing(4),
     },
     formControl: {
-        minWidth: 120,
         width: '100%',
     },
+    select: {
+        width: "min-content"
+    }
 }));
 
 // TODO: VALIDATION !!!
 
-const Payment = ({dispatch, history}) => {
+const Payment = ({user}) => {
     const classes = useStyles();
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [phone, setPhone] = useState('');
-    const [gender, setGender] = useState('');
-    const [errorMessage, setErrorMessage] = useState(null);
+    // let bt_instance;
+    // const [clientToken, setClientToken] = useState('576587648645');
 
-    const onFirstNameChange = firstName => {
-        // TODO: validate
-        // TODO: set error message if needed
-        setFirstName(firstName);
-    };
 
-    const onLastNameChange = lastName => {
-        // TODO: validate
-        // TODO: set error message if needed
-        setLastName(lastName);
-    };
-
-    const onEmailChange = email => {
-        // TODO: validate
-        // TODO: set error message if needed
-        setEmail(email);
-    };
-
-    const onPasswordChange = password => {
-        // TODO: validate
-        // TODO: set error message if needed
-        setPassword(password);
-    };
-
-    const onConfirmPasswordChange = confirmPassword => {
-        // TODO: validate
-        // TODO: set error message if needed
-        setConfirmPassword(confirmPassword);
-    };
-
-    const onPhoneChange = phone => {
-        // TODO: validate
-        // TODO: set error message if needed
-        setPhone(phone);
-    };
-
-    const onGenderChange = gender => {
-        // TODO: validate
-        // TODO: set error message if needed
-        setGender(gender);
-    };
-
-    const createAccountAction = async event => {
-        event.preventDefault();
-        const user = {
-            firstName, lastName, password, email, gender, phone
-        }
-        await dispatch(createAccount(user));
-        history.push('/home');
-    };
+    // const [firstName, setFirstName] = useState('');
+    // const [lastName, setLastName] = useState('');
+    // const [street, setStreet] = useState('');
+    // const [city, setCity] = useState('');
+    // const [phone, setPhone] = useState('');
+    // const [floor, setFloor] = useState('');
+    // const [errorMessage, setErrorMessage] = useState(null);
+    //
+    // const onFirstNameChange = firstName => {
+    //     // TODO: validate
+    //     // TODO: set error message if needed
+    //     setFirstName(firstName);
+    // };
+    //
+    // const onLastNameChange = lastName => {
+    //     // TODO: validate
+    //     // TODO: set error message if needed
+    //     setLastName(lastName);
+    // };
+    //
+    // const onStreetChange = street => {
+    //     // TODO: validate
+    //     // TODO: set error message if needed
+    //     setStreet(street);
+    // };
+    //
+    // const onCityChange = city => {
+    //     // TODO: validate
+    //     // TODO: set error message if needed
+    //     setCity(city);
+    // };
+    //
+    // const onPhoneChange = phone => {
+    //     // TODO: validate
+    //     // TODO: set error message if needed
+    //     setPhone(phone);
+    // };
+    //
+    // const onFloorChange = floor => {
+    //     // TODO: validate
+    //     // TODO: set error message if needed
+    //     setFloor(floor);
+    // };
+    //
+    // const createAccountAction = async event => {
+    //
+    // };
 
     return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
+        <Container component="main">
+            <CssBaseline/>
             <div className={classes.paper}>
                 <Avatar className={classes.avatar}>
                     <PaymentIcon/>
@@ -113,98 +99,110 @@ const Payment = ({dispatch, history}) => {
                     Payment
                 </Typography>
                 <form className={classes.form}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="fname"
-                                name="firstName"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="First Name"
-                                autoFocus
-                                value={firstName}
-                                onChange={event => onFirstNameChange(event.target.value)}
-                                error={!isEmpty(errorMessage)} // TODO
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="lname"
-                                value={lastName}
-                                onChange={event => onLastNameChange(event.target.value)}
-                                error={!isEmpty(errorMessage)} // TODO
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                value={email}
-                                onChange={event => onEmailChange(event.target.value)}
-                                error={!isEmpty(errorMessage)} // TODO
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                value={password}
-                                onChange={event => onPasswordChange(event.target.value)}
-                                error={!isEmpty(errorMessage)} // TODO
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="confirm-password"
-                                label="Confirm password"
-                                type="password"
-                                id="confirm-password"
-                                value={confirmPassword}
-                                onChange={event => onConfirmPasswordChange(event.target.value)}
-                                error={!isEmpty(errorMessage)} // TODO
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="phone"
-                                label="Phone"
-                                type="phone"
-                                id="phone"
-                                autoComplete="phone"
-                                value={phone}
-                                onChange={event => onPhoneChange(event.target.value)}
-                                error={!isEmpty(errorMessage)} // TODO
-                            />
-                        </Grid>
-                    </Grid>
+                    {/*//             <Grid container spacing={2}>*/}
+                    {/*//                 <Grid item xs={12} sm={6}>*/}
+                    {/*//                     <TextField*/}
+                    {/*//                         autoComplete="fname"*/}
+                    {/*//                         name="firstName"*/}
+                    {/*//                         variant="outlined"*/}
+                    {/*//                         required*/}
+                    {/*//                         fullWidth*/}
+                    {/*//                         id="firstName"*/}
+                    {/*//                         label="First Name"*/}
+                    {/*//                         autoFocus*/}
+                    {/*//                         value={firstName || user.firstName}*/}
+                    {/*//                         onChange={event => onFirstNameChange(event.target.value)}*/}
+                    {/*//                         error={!isEmpty(errorMessage)} // TODO*/}
+                    {/*//                     />*/}
+                    {/*//                 </Grid>*/}
+                    {/*//                 <Grid item xs={12} sm={6}>*/}
+                    {/*//                     <TextField*/}
+                    {/*//                         variant="outlined"*/}
+                    {/*//                         required*/}
+                    {/*//                         fullWidth*/}
+                    {/*//                         id="lastName"*/}
+                    {/*//                         label="Last Name"*/}
+                    {/*//                         name="lastName"*/}
+                    {/*//                         autoComplete="lname"*/}
+                    {/*//                         value={lastName || user.lastName}*/}
+                    {/*//                         onChange={event => onLastNameChange(event.target.value)}*/}
+                    {/*//                         error={!isEmpty(errorMessage)} // TODO*/}
+                    {/*//                     />*/}
+                    {/*//                 </Grid>*/}
+                    {/*//                 <Grid item xs={12} sm={6}>*/}
+                    {/*//                     <TextField*/}
+                    {/*//                         variant="outlined"*/}
+                    {/*//                         required*/}
+                    {/*//                         fullWidth*/}
+                    {/*//                         id="street"*/}
+                    {/*//                         label="Street"*/}
+                    {/*//                         name="street"*/}
+                    {/*//                         autoComplete="street"*/}
+                    {/*//                         value={street}*/}
+                    {/*//                         onChange={event => onStreetChange(event.target.value)}*/}
+                    {/*//                         error={!isEmpty(errorMessage)} // TODO*/}
+                    {/*//                     />*/}
+                    {/*//                 </Grid>*/}
+                    {/*//                 <Grid item xs={12} sm={6}>*/}
+                    {/*//                     <TextField*/}
+                    {/*//                         variant="outlined"*/}
+                    {/*//                         required*/}
+                    {/*//                         fullWidth*/}
+                    {/*//                         name="city"*/}
+                    {/*//                         label="City"*/}
+                    {/*//                         id="city"*/}
+                    {/*//                         value={city}*/}
+                    {/*//                         onChange={event => onCityChange(event.target.value)}*/}
+                    {/*//                         error={!isEmpty(errorMessage)} // TODO*/}
+                    {/*//                     />*/}
+                    {/*//                 </Grid>*/}
+                    {/*//                 <Grid item xs={12} sm={6}>*/}
+                    {/*//                     <TextField*/}
+                    {/*//                         variant="outlined"*/}
+                    {/*//                         required*/}
+                    {/*//                         fullWidth*/}
+                    {/*//                         name="phone"*/}
+                    {/*//                         label="Phone"*/}
+                    {/*//                         type="phone"*/}
+                    {/*//                         id="phone"*/}
+                    {/*//                         autoComplete="phone"*/}
+                    {/*//                         value={phone || user.phone}*/}
+                    {/*//                         onChange={event => onPhoneChange(event.target.value)}*/}
+                    {/*//                         error={!isEmpty(errorMessage)} // TODO*/}
+                    {/*//                     />*/}
+                    {/*//                 </Grid>*/}
+                    {/*//                 <Grid item xs={12} sm={6}>*/}
+                    {/*//                     <FormControl variant="outlined" className={classes.formControl}>*/}
+                    {/*//                         <InputLabel id="floor-select-outlined-label">Floor</InputLabel>*/}
+                    {/*//                         <Select*/}
+                    {/*//                             labelId="select-floor-label"*/}
+                    {/*//                             id="floor-select"*/}
+                    {/*//                             value={floor}*/}
+                    {/*//                             onChange={event => onFloorChange(event.target.value)}*/}
+                    {/*//                             error={!isEmpty(errorMessage)} // TODO*/}
+                    {/*//                             label="Floor"*/}
+                    {/*//                             autoWidth*/}
+                    {/*//                         >*/}
+                    {/*//                             {*/}
+                    {/*//                                 FLOORS.map(f =>*/}
+                    {/*//                                     <MenuItem value={f}>{f}</MenuItem>*/}
+                    {/*//                                 )*/}
+                    {/*//                             }*/}
+                    {/*//                         </Select>*/}
+                    {/*//                     </FormControl>*/}
+                    {/*//                 </Grid>*/}
+                    {/*//             </Grid>*/}
+                    {/*//*/}
+                    {/*{clientToken ? <div>*/}
+                    {/*    <DropIn*/}
+                    {/*        options={{authorization: clientToken}}*/}
+                    {/*        onInstance={(instance) => (this.instance = instance)}*/}
+                    {/*    />*/}
+                    {/*</div> : <div>Loading</div>}*/}
                 </form>
             </div>
         </Container>
     );
 };
 
-export default connect(store => ({}))(Payment);
+export default Payment;
